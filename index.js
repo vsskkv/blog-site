ca10675bdfceabc64081a2070ccc03d8ba2732c8
vsskkv <you@example.com>
2019-10-20 18:20:07 +0100

    added static pages to the pages folder

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ app.get('/', (req, res) => {
 
 })
 
+app.get('/about', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+
+})
+
+app.get('/post', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+
+})
+
+app.get('/contact', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+
+})
+
+
 app.listen(4000, () => {
 
     console.log('App listing on port 4000')
