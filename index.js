5327f000c6da88d5ea7a93c6fb8a58c897045dc8
vikram <vsskkv@gmail.com>
2019-10-21 10:22:13 +0100

    refactored pages with express-edge

--- index.js
+++ index.js
@@ -2,31 +2,37 @@
 const express = require('express')
 const path = require('path')
 
+const { engine } = require('express-edge')
+
 const app = new express()
 
 app.use(express.static('public'))
 
+app.use(engine)
+
+app.set('views', `${__dirname}/views`);
+
 app.get('/', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
 
 })
 
 app.get('/about', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
 
 })
 
 app.get('/post', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
 
 })
 
 app.get('/contact', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
 
 })
 
