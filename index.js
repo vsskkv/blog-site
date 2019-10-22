238ce1b008d245e73652838b8908c8b4b9f7b18e
vikram <vsskkv@gmail.com>
2019-10-22 11:43:19 +0100

    users login started

--- index.js
+++ index.js
@@ -1,44 +1,75 @@
-//blog site
-const express = require('express')
-const path = require('path')
-const { engine } = require('express-edge')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const fileupload = require('express-fileupload')
+require('dotenv').config();
+const expressEdge = require("express-edge");
+const express = require("express");
+const edge = require("edge.js");
+const cloudinary = require('cloudinary');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const expressSession = require("express-session");
+const connectMongo = require("connect-mongo");
+const connectFlash = require("connect-flash");
 
-const Post = require('./database/models/Post')
+const createPostController = require("./controllers/createPost");
+const homePageController = require("./controllers/homePage");
+const storePostController = require("./controllers/storePost");
+const getPostController = require("./controllers/getPost");
+const createUserController = require("./controllers/createUser");
+const storeUserController = require("./controllers/storeUser");
+const loginController = require("./controllers/login");
+const loginUserController = require("./controllers/loginUser");
+const logoutController = require("./controllers/logout");
 
-const createPostController = require('./controllers/createPost')
-const homePagecontroller = require('./controllers/homePage')
-const storePostcontroller = require('./controllers/storePost')
-const getPostController = require('./controllers/getPost')
+const app = new express();
+mongoose.connect(process.env.DB_URI);
 
-const app = new express()
+app.use(connectFlash());
 
-mongoose.connect('mongodb://localhost/node-js-blog', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-})
+cloudinary.config({
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+});
 
-app.use(fileupload())
-app.use(express.static('public'))
-app.use(engine)
-app.set('views', `${__dirname}/views`);
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const mongoStore = connectMongo(expressSession);
 
-app.get('/', homePagecontroller)
+app.use(
+  expressSession({
+    secret: process.env.EXPRESS_SESSION_KEY,
+    store: new mongoStore({
+      mongooseConnection: mongoose.connection
+    })
+  })
+);
 
-app.get('/post/:id', getPostController)
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(expressEdge);
+app.set("views", `${__dirname}/views`);
 
-app.get('/post/new', createPostController)
+app.use("*", (req, res, next) => {
+  edge.global("auth", req.session.userId);
+  next();
+});
 
-app.post('/post/store', storePostcontroller)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const storePost = require("./middleware/storePost");
+const auth = require("./middleware/auth");
+const redirectIfAuthenticated = require("./middleware/redirectIfAuthenticated");
 
+app.get("/", homePageController);
+app.get("/post/:id", getPostController);
+app.get("/auth/logout", auth, logoutController);
+app.get("/posts/new", auth, createPostController);
+app.post("/posts/store", auth, storePost, storePostController);
+app.get("/auth/login", redirectIfAuthenticated, loginController);
+app.post("/users/login", redirectIfAuthenticated, loginUserController);
+app.get("/auth/register", redirectIfAuthenticated, createUserController);
+app.post("/users/register", redirectIfAuthenticated, storeUserController);
+app.use((req, res) => res.render('not-found'));
 
-app.listen(4000, () => {
-
-    console.log('App listing on port 4000')
-
-})
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
+});
